f1dd6a0fe3ab176f4c71a053eac0b3cbb3dbce0e
Antonio Luis <antonio_luis@outlook.com>
2024-04-09 09:29:05 -0300

    fix: change default theme

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "../i18n/i18n";
 import { ThemeProvider } from "@lib";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <App />
     </ThemeProvider>
   </React.StrictMode>
